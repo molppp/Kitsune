20c69cb1a6a2b9b4a36abe76ea6a1a97796cb170
molp <142534170+molppp@users.noreply.github.com>
2024-01-10 19:22:07 +0300

    Update Footer.tsx

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -7,10 +7,10 @@ export const Footer = (props: {}) => {
     <footer className="w-full bg-base-300 shadow-xl p-5 flex flex-col items-center space-y-5">
       <img src="/icon.png" alt="logo" width="100" height="100" />
       <div className="flex space-x-5 items-center">
-        <a href="https://github.com/Dovakiin0/Kitsune" target="_blank">
+        <a href="" target="_blank">
           <FaGithub size={25} />
         </a>
-        <a href="https://discord.gg/6yAJ3XDHTt" target="_blank">
+        <a href="https://discord.gg/" target="_blank">
           <FaDiscord size={25} />
         </a>
       </div>
